62fa24449da18765067508e7c24f3ddccf3b8a78
Malted <aluminumcrab@gmail.com>
2016-05-20 00:09:42 -0500

    Reads in the tiles in one pass.

--- src/REXSpeeder.cpp
+++ src/REXSpeeder.cpp
@@ -2,8 +2,12 @@
 #include "zlib.h"
 
 namespace xp {
+
+//===========================================================================================================//
+//    Loading an xp file                                                                                     //
+//===========================================================================================================//
 	RexFile::RexFile(std::string const & filename) {
-		const int tileLen = 10; //number of bytes in a tile. Probably not equal to sizeof(RexTile) due to padding.
+		const int tileLen = 10; //Number of bytes in a tile. Not equal to sizeof(RexTile) due to padding.
 
 		gzFile gz = gzopen(filename.c_str(), "rb");
 
@@ -17,14 +21,11 @@ namespace xp {
 			for (int i = 0; i < num_layers; i++)
 				layers[i] = new RexLayer(width, height);
 
-
 			for (int layer_index = 0; layer_index < num_layers; layer_index++) {
 				RexLayer* layer = layers[layer_index];
-				for (int x = 0; x < width; x++) {
-					for (int y = 0; y < height; y++) {
-						RexTile* tile = &layer->tiles[x + (y * width)];
-						gzread(gz, tile, tileLen);
-					}
+				for (int i = 0; i < width*height; ++i){
+					RexTile* tile = &layer->tiles[i];
+					gzread(gz, tile, tileLen);
 				}
 
 				//The layer and height information is repeated.
@@ -40,19 +41,9 @@ namespace xp {
 		gzclose(gz);
 	}
 
-	RexFile::RexFile(int _version, int _width, int _height, int _num_layers)
-		:version(_version), width(_width), height(_height), num_layers(_num_layers) {
-		for (int i = 0; i < num_layers; i++) {
-			layers[i] = new RexLayer(width, height);
-		}
-	}
-
-	RexFile::~RexFile() {
-		for (int i = 0; i < num_layers; i++) {
-			delete(layers[i]);
-		}
-	}
-
+//===========================================================================================================//
+//    Saving an xp file                                                                                      //
+//===========================================================================================================//
 	void RexFile::save(std::string const & filename) {
 		typedef void* vp;
 		const int color_size = sizeof(RexTile::fore_red);
@@ -67,24 +58,38 @@ namespace xp {
 			gzwrite(gz, (vp)&width, sizeof(width));
 			gzwrite(gz, (vp)&height, sizeof(height));
 
-			for (int x = 0; x < width; x++) {
-				for (int y = 0; y < height; y++) {
-					RexTile* tile = getTile(layer, x, y);
-					//Character
-					gzwrite(gz, (vp)&tile->character, chara_size);
-					//Foreground
-					gzwrite(gz, (vp)&tile->fore_red, color_size);
-					gzwrite(gz, (vp)&tile->fore_green, color_size);
-					gzwrite(gz, (vp)&tile->fore_blue, color_size);
-					//Background
-					gzwrite(gz, (vp)&tile->back_red, color_size);
-					gzwrite(gz, (vp)&tile->back_green, color_size);
-					gzwrite(gz, (vp)&tile->back_blue, color_size);
-				}
+			for (int i = 0; i < width*height; ++i) {
+				RexTile* tile = &layers[layer]->tiles[i];
+				//Character
+				gzwrite(gz, (vp)&tile->character, chara_size);
+				//Foreground
+				gzwrite(gz, (vp)&tile->fore_red, color_size);
+				gzwrite(gz, (vp)&tile->fore_green, color_size);
+				gzwrite(gz, (vp)&tile->fore_blue, color_size);
+				//Background
+				gzwrite(gz, (vp)&tile->back_red, color_size);
+				gzwrite(gz, (vp)&tile->back_green, color_size);
+				gzwrite(gz, (vp)&tile->back_blue, color_size);
 			}
 		}
 
 		gzflush(gz, Z_FULL_FLUSH);
 		gzclose(gz);
+	}
+
+//===========================================================================================================//
+//    Constructors / Destructors                                                                             //
+//===========================================================================================================//
+	RexFile::RexFile(int _version, int _width, int _height, int _num_layers)
+		:version(_version), width(_width), height(_height), num_layers(_num_layers) {
+		for (int i = 0; i < num_layers; i++) {
+			layers[i] = new RexLayer(width, height);
+		}
+	}
+
+	RexFile::~RexFile() {
+		for (int i = 0; i < num_layers; i++) {
+			delete(layers[i]);
+		}
 	}
 }
